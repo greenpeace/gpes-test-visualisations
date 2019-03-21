2ef589a4f936150bddf60477373cd1ce2e2a57cb
Osvaldo Gago <opgago@yahoo.com>
2019-03-21 16:51:10 +0100

    Remove unnecessary comments and line break

--- _includes/media-1/scripts.js
+++ _includes/media-1/scripts.js
@@ -1,5 +1,4 @@
  document.addEventListener("DOMContentLoaded", function (t) {
-    /* BEGIN SCRIPTS */
 
     var s, e = "https://storage.googleapis.com/gpes-static/plasticos/plasticos-360.svg",
         o = "https://storage.googleapis.com/gpes-static/plasticos/plasticos-510.svg",
@@ -12,5 +11,4 @@
     var l = document.createElement("object");
     l.setAttribute("type", "image/svg+xml"), l.setAttribute("width", c), l.setAttribute("height", "auto"), l.setAttribute("data", s), g.appendChild(l)
 
-    /* END SCRIPTS */
 });
